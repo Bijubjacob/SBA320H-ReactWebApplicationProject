4e6e0e1f45d80e08fe776d2385bb2e77f927b5ef
Bijubjacob <bijubjacob@gmail.com>
2025-02-17 22:45:44 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Footer from './components/Footer';
+import './App.css';
+import './components/Footer.css';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        < Footer />
+      </div>
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="footer">
+            Copyright @2025
+        </footer>
+    )
+}
+
+export default Footer;
